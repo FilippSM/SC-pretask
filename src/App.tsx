00b1aa568a1cd651275732f15cd7d089e39288ea
FilippSM_laptop <philippsm666@gmail.com>
2024-10-09 23:38:02 +0300

    add img

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import styled from 'styled-components';
+import rectangleimage from './Rectangle.png'
 
 
 function App() {
   return (
     <div className="App">
       <Box>
-        <Card></Card>
+        <Card>
+          <Img src={rectangleimage} alt=""></Img>
+        </Card>
       </Box>
 
     </div>
   );
 }
 
-//создание стилизованного компонента
-/* const StyleBtn = styled.button`
-border: none;
-background-color: #ab1818;
-padding: 10px 20px;
-border-radius: 5px;
-color: snow;
-font-size: 2 rem;
-font-weight: bold;
-` */
-
 const Card = styled.div`
   border-radius: 15px;
   width: 300px;
   height: 350px;
+  padding: 10px;
 
   box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.1);
   background: #fff;
 `
+const Img = styled.img`
+  border: 1px solid #000;
+  border-radius: 10px;
+  width: 280px;
+  height: 170px;
+`
+
 
 //создание враппера
 const Box = styled.div`
