bfa45635075f3048ca7ca498e698dac51e581f5d
FilippSM_laptop <philippsm666@gmail.com>
2024-10-10 23:17:34 +0300

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,12 @@ function App() {
       <Box>
         <Card>
           <Img src={rectangleimage} alt=""></Img>
+          <h2>Headline</h2>
+          <p>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</p>
+          <ButtonOne>See more</ButtonOne>
+          <ButtonTwo>Save</ButtonTwo>
         </Card>
       </Box>
-
     </div>
   );
 }
@@ -25,13 +28,50 @@ const Card = styled.div`
 
   box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.1);
   background: #fff;
-`
+
+  p {
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 1.7;
+    color: #abb3ba;
+    margin: 20px 10px;
+  }
+
+  h2 {
+    font-weight: 700;
+    font-size: 16px;
+    color: #000;
+    margin: 20px 10px;
+  }
+`;
+
 const Img = styled.img`
   border: 1px solid #000;
   border-radius: 10px;
   width: 280px;
   height: 170px;
-`
+`;
+
+const ButtonOne = styled.button`
+  border: none;
+  background: #4e71fe;
+  border-radius: 5px;
+  width: 86px;
+  height: 30px;
+  margin: 0px 0px 0px 10px;
+
+  font-weight: 700;
+  font-size: 10px;
+  line-height: 2;
+  color: #fff;
+`;
+
+const ButtonTwo = styled(ButtonOne)`
+  border: 2px solid #4e71fe;
+  background: none;
+  color: #4e71fe;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+`;
 
 
 //создание враппера
@@ -40,6 +80,6 @@ const Box = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 export default App;
